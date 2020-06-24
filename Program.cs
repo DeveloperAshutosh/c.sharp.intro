5842416c3e18835c2f0e3bf6022bd606b4f3cf8a
dimple sharma <dimplessharma@hotmail.com>
2020-06-24 15:14:29 -0600

    add and assignment; output concatinated hello string.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace c.sharp.intro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, TECHCareers");
+    
+            // two slashes mark the beginning of single line comment.
+            // we are asigning string value to variable
+            string myName = "Ashutosh Kaushal";
+            /*we begin multi-line comments with an forward slash and astrisk
+            
+
+            and we end our multi-line comments with an astersik and forward slash
+            */
+            Console.WriteLine("Hello, " + myName);
         }
     }
 }
