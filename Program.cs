3a605fb84fbd508009a84fbdd9d6c5a898b4305e
dimple sharma <dimplessharma@hotmail.com>
2020-06-24 15:58:41 -0600

    learned some new string functions

--- Program.cs
+++ Program.cs
@@ -20,8 +20,19 @@ namespace c.sharp.intro
 
             // assign variable 'username to a value entered bu the user
             string userName = Console.ReadLine();
+            // show the user that we collected the name.
             Console.WriteLine("you have entered: " + userName);
+            //instead of tradional concatination using the addition operator...
+            //we can use string interpolation (placeholders) to embed values 
+            //in string
             Console.WriteLine("Hey {0} you are looking great today", userName);
+            //lets play with string
+            Console.WriteLine( "makemeuppercase".ToUpper());
+            Console.WriteLine("MAKEMELOWERCASE".ToLower());
+            // substrings take two arguements 1)string chracter position 2) number of
+            //chracters to collect
+            Console.WriteLine(" ABCDEFGHIJKLMNOPQRSTUVWXYZ".Substring( 3 , 3));
+            Console.WriteLine("XYZ".Length);//GET THE NUMBER OF CHARACTERS FROM THE STRING
 
         }
     }
