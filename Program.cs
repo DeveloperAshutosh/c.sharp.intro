d0d2235d94e9b328580d73c94fa932693262b9c0
dimple sharma <dimplessharma@hotmail.com>
2020-06-25 14:06:48 -0600

    added substraction - multiplication

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace c.sharp.intro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("please tell us if you want to add or substract: (add/subsract");
+            Console.WriteLine("please tell us if you want to add , substract or multiply: (add/subsract/multiply");
             string userOperator = Console.ReadLine();
             //check if the user enterd add.
             if (userOperator =="add")
@@ -24,6 +24,43 @@ namespace c.sharp.intro
                 int result = firstNum + secondNum ;
 
                 Console.WriteLine("{0} +{1}={2}", firstNum ,secondNum , result );
+            }
+            else if( userOperator == "substract")
+            {
+                Console.WriteLine("Please enter the first number");
+                //https://stackoverflow.com/questions/1019793/how-can-i-convert-string-to-int
+
+
+                int firstNum = Int32.Parse( Console.ReadLine() );
+
+                Console.WriteLine("Please enter the second number");
+                //calculate the result.
+
+                int secondNum = Int32.Parse( Console.ReadLine() );
+                int result = firstNum - secondNum ;
+
+                Console.WriteLine("{0} - {1} = {2}", firstNum ,secondNum , result );
+
+            }
+            else if(userOperator == "multiply")
+            {
+                Console.WriteLine("Please enter the first number");
+                //https://stackoverflow.com/questions/1019793/how-can-i-convert-string-to-int
+
+
+                int firstNum = Int32.Parse( Console.ReadLine() );
+
+                Console.WriteLine("Please enter the second number");
+                //calculate the result.
+
+                int secondNum = Int32.Parse( Console.ReadLine() );
+                int result = firstNum * secondNum ;
+
+                Console.WriteLine("{0} *{1}={2}", firstNum ,secondNum , result );
+            }
+            else 
+            {
+                Console.WriteLine("invalid operator added");
             }
 
    
